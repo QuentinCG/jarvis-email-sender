14ee5ccd44fdb1ef394e3086818281f100ce15b8
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-19 15:00:45 +0200

    install/uninstall: Add python install and add mail sender python script

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-# Use only if you need to perform changes on the user system such as installing software
+#!/usr/bin/env bash
+
+# Install Python (python 2.7 and 3.x)
+[[ -z $(which python) ]] && sudo apt-get install python
+[[ -z $(which python2.7) ]] && sudo apt-get install python2.7
+[[ -z $(which python3) ]] && sudo apt-get install python3
+
+# Copy latest version of "send email" python script
+wget -P script/ https://raw.githubusercontent.com/QuentinCG/Base-Scripts/master/OS_Independent/utils/send_email.py
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-# Use only if you need to undo changes on the user system such as removing software
+#!/usr/bin/env bash
+
+# Delete old version of "send email" python script
+rm script/send_email.py
