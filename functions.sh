2b69ed0643252ea1e6cea6d2af9ae39718425506
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-19 15:51:57 +0200

    plugin: Add base implementation (not working yet)

--- functions.sh
+++ functions.sh
@@ -1,8 +1,18 @@
-#!/bin/bash
-# Here you can create functions which will be available from the commands file
-# You can also use here user variables defined in your config file
-# To avoid conflicts, name your function like this
-# pg_XX_myfunction () { }
-# pg for PluGin
-# XX is a short code for your plugin, ex: ww for Weather Wunderground
-# You can use translations provided in the language folders functions.sh
+#!/usr/bin/env bash
+
+# Send Email
+# $1 (string): To addresses
+# $2 (string): Subject
+# $3 (string): Message
+jv_pg_es_send_email()
+{
+  # Send request to SMTP server
+  result="$(python plugins/jarvis-email-sender/script/send_email.py --host $var_jv_pg_es_host --port $var_jv_pg_es_port --using_tls $var_jv_pg_es_using_tls --username $var_jv_pg_es_username --password $var_jv_pg_es_password --fromAddr $var_jv_pg_es_from_address --fromName $var_jv_pg_es_from_name --toAddressesAndNames $1 --subject $2 --message $3)"
+
+  # Show the result to user
+  if [[ $result =~ "True" ]]; then
+    say "$(jv_pg_es_lang send_email_success)"
+  else
+    say "$(jv_pg_es_lang send_email_failed)"
+  fi
+}
