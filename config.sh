b643eb3d819c1af7bfb3a89fba4ff19df2c9326c
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-19 15:51:22 +0200

    config: Add base config for the plugin (login, password, smtp server, ...)

---
+++ config.sh
@@ -0,0 +1,12 @@
+var_jv_pg_es_host="smtp.gmail.com" # SMTP host name
+var_jv_pg_es_port=587  # SMTP port
+var_jv_pg_es_using_tls=True # Enable TLS security (True/False)
+var_jv_pg_es_username="username_here"
+var_jv_pg_es_password="password"
+var_jv_pg_es_from_address="dummyfromaddress@domain.com"
+var_jv_pg_es_from_name="name_of_dummy_from_address"
+
+# List of receivers (used in command file)
+var_jv_pg_es_receivers_1="[[\"receiver1@domain.com\", \"receiver 1\"], [\"receiver2@domain.com\", \"receiver 2\"]]"
+var_jv_pg_es_receivers_2="[[\"receiver3@domain.com\", \"receiver 3\"], [\"receiver4@domain.com\", \"receiver 4\"]]"
+# ...
