a6c421cc841bb45fef564f9f0fa4753b3b602d61
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-20 10:53:45 +0200

    plugin: Improve implementation

--- functions.sh
+++ functions.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Send Email
-# $1 (string): To addresses
+# $1 (string): To addresses (example: "[[\"receiver1@domain.com\", \"receiver 1\"], [\"receiver2@domain.com\", \"receiver 2\"]]")
 # $2 (string): Subject
 # $3 (string): Message
 jv_pg_es_send_email()
 {
   # Send request to SMTP server
-  result="$(python plugins/jarvis-email-sender/script/send_email.py --host $var_jv_pg_es_host --port $var_jv_pg_es_port --using_tls $var_jv_pg_es_using_tls --username $var_jv_pg_es_username --password $var_jv_pg_es_password --fromAddr $var_jv_pg_es_from_address --fromName $var_jv_pg_es_from_name --toAddressesAndNames $1 --subject $2 --message $3)"
+  result=`python plugins/jarvis-email-sender/script/send_email.py --host $var_jv_pg_es_host --port $var_jv_pg_es_port --using_tls $var_jv_pg_es_using_tls --username $var_jv_pg_es_username --password $var_jv_pg_es_password --fromAddr $var_jv_pg_es_from_address --fromName $var_jv_pg_es_from_name --toAddressesAndNames $1 --subject "$2" --message "$3"`
 
   # Show the result to user
-  if [[ $result =~ "True" ]]; then
+  if [[ $result =~ "Send email: True" ]]; then
     say "$(jv_pg_es_lang send_email_success)"
   else
     say "$(jv_pg_es_lang send_email_failed)"
