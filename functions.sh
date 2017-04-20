d9a737ab46607ae61bc79644d5f092f7f9731bd0
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-20 11:15:40 +0200

    plugin: Fix last issue to have a working plugin

--- functions.sh
+++ functions.sh
@@ -7,7 +7,7 @@
 jv_pg_es_send_email()
 {
   # Send request to SMTP server
-  result=`python plugins/jarvis-email-sender/script/send_email.py --host $var_jv_pg_es_host --port $var_jv_pg_es_port --using_tls $var_jv_pg_es_using_tls --username $var_jv_pg_es_username --password $var_jv_pg_es_password --fromAddr $var_jv_pg_es_from_address --fromName $var_jv_pg_es_from_name --toAddressesAndNames $1 --subject "$2" --message "$3"`
+  result=`python plugins/jarvis-email-sender/script/send_email.py --host "$var_jv_pg_es_host" --port $var_jv_pg_es_port --usingTls "$var_jv_pg_es_using_tls" --username "$var_jv_pg_es_username" --password "$var_jv_pg_es_password" --fromAddr "$var_jv_pg_es_from_address" --fromName "$var_jv_pg_es_from_name" --toAddressesAndNames "$1" --subject "$2" --message "$3"`
 
   # Show the result to user
   if [[ $result =~ "Send email: True" ]]; then
