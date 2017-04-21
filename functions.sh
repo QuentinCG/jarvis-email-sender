23ff4112ddbc335be7adf8e704335b860178bb5d
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-21 10:55:28 +0200

    plugin: Add attachement functionnality

--- functions.sh
+++ functions.sh
@@ -4,10 +4,12 @@
 # $1 (string): To addresses (example: "[[\"receiver1@domain.com\", \"receiver 1\"], [\"receiver2@domain.com\", \"receiver 2\"]]")
 # $2 (string): Subject
 # $3 (string): Message
+# $4 (string): Attachement files (example: "[\"/tmp/test.pdf\", \"/tmp/test2.avi\")
+# $5 (string): Attachement pictures (example: "[\"/tmp/test.jpg\", \"/tmp/test2.gif\")
 jv_pg_es_send_email()
 {
   # Send request to SMTP server
-  result=`python plugins/jarvis-email-sender/script/send_email.py --host "$var_jv_pg_es_host" --port $var_jv_pg_es_port --usingTls "$var_jv_pg_es_using_tls" --username "$var_jv_pg_es_username" --password "$var_jv_pg_es_password" --fromAddr "$var_jv_pg_es_from_address" --fromName "$var_jv_pg_es_from_name" --toAddressesAndNames "$1" --subject "$2" --message "$3"`
+  result=`python3 plugins/jarvis-email-sender/script/send_email.py --host "$var_jv_pg_es_host" --port $var_jv_pg_es_port --usingTls "$var_jv_pg_es_using_tls" --username "$var_jv_pg_es_username" --password "$var_jv_pg_es_password" --fromAddr "$var_jv_pg_es_from_address" --fromName "$var_jv_pg_es_from_name" --toAddressesAndNames "$1" --subject "$2" --message "$3" --attachements "$4" --images "$5"`
 
   # Show the result to user
   if [[ $result =~ "Send email: True" ]]; then
