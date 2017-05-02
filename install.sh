406cf06d36c85805d9ab39c47a2ae51391985061
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-02 11:24:56 +0200

    install: Have automatic install

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Install Python (python 2.7 and 3.x)
-[[ -z $(which python) ]] && sudo apt-get install python
-[[ -z $(which python2.7) ]] && sudo apt-get install python2.7
-[[ -z $(which python3) ]] && sudo apt-get install python3
+[[ -z $(which python) ]] && sudo apt-get --yes --force-yes install python
+[[ -z $(which python2.7) ]] && sudo apt-get --yes --force-yes install python2.7
+[[ -z $(which python3) ]] && sudo apt-get --yes --force-yes install python3
 
 # Copy latest version of "send email" python script
 wget -P script/ https://raw.githubusercontent.com/QuentinCG/Base-Scripts/master/OS_Independent/utils/send_email.py
